3bdab358ff9fbf23f0ce07e5d1a92b58ca4aead2
gnuhub <gnuhub@gmail.com>
2022-11-07 17:30:06 +0800

    auto commit

--- 6.ci.sh
+++ 6.ci.sh
@@ -8,24 +8,29 @@ export PROJECT_NAME="${CMD_PATH##*/}"
 
 cd $CMD_PATH
 
-ssh-keygen -f "$HOME/.ssh/known_hosts" -R "github.com"
+cp -fv known_hosts /root/.ssh/known_hosts
+ssh-keygen -f "/root/.ssh/known_hosts" -R "frs.sourceforge.net"
+ssh-keyscan "frs.sourceforge.net" >> /root/.ssh/known_hosts
+
+
+ssh-keygen -f "/root/.ssh/known_hosts" -R "github.com"
 ssh-keyscan "github.com" >> ~/.ssh/known_hosts
+cat /root/.ssh/known_hosts
 
-
-mkdir -p $HOME/git
-cd $HOME/git
+mkdir -p /root/git
+cd /root/git
 git clone https://github.com/archlinux/aur.git 9317-archlinux-aur
 
 git clone git@github.com:archlinux365/9317-archlinux-aur.git
 
-cd $HOME/git/9317-archlinux-aur
+cd /root/git/9317-archlinux-aur
 git remote add origin  git@github.com:archlinux365/9317-archlinux-aur.git
 git remote set-url origin git@github.com:archlinux365/9317-archlinux-aur.git
 
 git remote add upstream  https://github.com/archlinux/aur.git
 git remote set-url upstream https://github.com/archlinux/aur.git
 
-cd $HOME/git/9317-archlinux-aur/.git/refs/remotes/origin
+cd /root/git/9317-archlinux-aur/.git/refs/remotes/origin
 
 git config --global user.email "gnuhub@gmail.com"
 git config --global user.name "gnuhub"
@@ -33,15 +38,15 @@ git config --global user.name "gnuhub"
 for pkg in `ls`
 do 
     echo $pkg
-    cd $HOME/git/9317-archlinux-aur/
+    cd /root/git/9317-archlinux-aur/
     
    
     git checkout $pkg 
     git push origin :$pkg
-    rsync -avP $CMD_PATH/t/ $HOME/git/9317-archlinux-aur/
+    rsync -avP $CMD_PATH/t/ /root/git/9317-archlinux-aur/
     git add .
     git commit -a -m ""add ci template""
     git push origin $pkg
   
-    cd $HOME/git/9317-archlinux-aur/.git/refs/remotes/origin
+    cd /root/git/9317-archlinux-aur/.git/refs/remotes/origin
 done
